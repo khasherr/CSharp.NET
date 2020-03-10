d335f1f2ea46eda338173a34f91da24beae233b8
Sher <38057955+khasherr@users.noreply.github.com>
2020-03-10 12:10:13 -0400

    Flight Information System
    Flight information system using C# Win Form. 
    DB connected model with SQL Server.

---
+++ DBConnectedModel/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace DBConnectedModel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbAirline = new System.Windows.Forms.TextBox();
+            this.tbFlightNum = new System.Windows.Forms.TextBox();
+            this.tbDestination = new System.Windows.Forms.TextBox();
+            this.btnAddNew = new System.Windows.Forms.Button();
+            this.cmbSelect = new System.Windows.Forms.ComboBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbAirlineType = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Airline";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Flight Number";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Destination";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Airline Type";
+            // 
+            // tbAirline
+            // 
+            this.tbAirline.Location = new System.Drawing.Point(110, 63);
+            this.tbAirline.Name = "tbAirline";
+            this.tbAirline.Size = new System.Drawing.Size(100, 20);
+            this.tbAirline.TabIndex = 7;
+            // 
+            // tbFlightNum
+            // 
+            this.tbFlightNum.Location = new System.Drawing.Point(110, 92);
+            this.tbFlightNum.Name = "tbFlightNum";
+            this.tbFlightNum.Size = new System.Drawing.Size(100, 20);
+            this.tbFlightNum.TabIndex = 8;
+            // 
+            // tbDestination
+            // 
+            this.tbDestination.Location = new System.Drawing.Point(110, 120);
+            this.tbDestination.Name = "tbDestination";
+            this.tbDestination.Size = new System.Drawing.Size(100, 20);
+            this.tbDestination.TabIndex = 9;
+            // 
+            // btnAddNew
+            // 
+            this.btnAddNew.Location = new System.Drawing.Point(530, 55);
+            this.btnAddNew.Name = "btnAddNew";
+            this.btnAddNew.Size = new System.Drawing.Size(212, 24);
+            this.btnAddNew.TabIndex = 14;
+            this.btnAddNew.Text = "Add New";
+            this.btnAddNew.UseVisualStyleBackColor = true;
+            this.btnAddNew.Click += new System.EventHandler(this.btnAddNew_Click);
+            // 
+            // cmbSelect
+            // 
+            this.cmbSelect.FormattingEnabled = true;
+            this.cmbSelect.Location = new System.Drawing.Point(530, 112);
+            this.cmbSelect.Name = "cmbSelect";
+            this.cmbSelect.Size = new System.Drawing.Size(212, 21);
+            this.cmbSelect.TabIndex = 15;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(530, 164);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 16;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(667, 164);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 17;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(561, 414);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(212, 24);
+            this.btnRefresh.TabIndex = 18;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(81, 228);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(557, 150);
+            this.dataGridView1.TabIndex = 19;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(32, 185);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 20;
+            // 
+            // cmbAirlineType
+            // 
+            this.cmbAirlineType.FormattingEnabled = true;
+            this.cmbAirlineType.Items.AddRange(new object[] {
+            "Airbus 320",
+            "Boeing 720 ",
+            "Boeing 737 ",
+            "Boeing 747 ",
+            "Airbus 920"});
+            this.cmbAirlineType.Location = new System.Drawing.Point(110, 146);
+            this.cmbAirlineType.Name = "cmbAirlineType";
+            this.cmbAirlineType.Size = new System.Drawing.Size(100, 21);
+            this.cmbAirlineType.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmbAirlineType);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.cmbSelect);
+            this.Controls.Add(this.btnAddNew);
+            this.Controls.Add(this.tbDestination);
+            this.Controls.Add(this.tbFlightNum);
+            this.Controls.Add(this.tbAirline);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbAirline;
+        private System.Windows.Forms.TextBox tbFlightNum;
+        private System.Windows.Forms.TextBox tbDestination;
+        private System.Windows.Forms.Button btnAddNew;
+        private System.Windows.Forms.ComboBox cmbSelect;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbAirlineType;
+    }
+}
+
---
+++ DBConnectedModel/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DBConnectedModel
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection conn = new SqlConnection();
+        private String conString = @"Server=DESKTOP-KD5D2CF\KHANSQL; Database=NetClass1; User=Mike; Password=mike1234";
+        private SqlCommand cmd;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+
+
+        {
+
+            cmbAirlineType.SelectedIndex = 0;
+            refreshData();
+        }
+
+        private void refreshData()
+        {
+            //instantiate sql connection object
+            // SqlConnection conn = new SqlConnection();
+            //connection string - user must have permission to access the db
+            //conn.ConnectionString = @"Server=DESKTOP-KD5D2CF\KHANSQL; Database=NetClass1; User=Mike; Password=mike1234";
+
+            conn.ConnectionString = conString;
+            SqlCommand cmd = conn.CreateCommand();
+
+
+            //instantiate command object
+            cmd = conn.CreateCommand();
+
+            try
+            {
+                String query = "Select * From dbo.FlightInfo";
+                cmd.CommandText = query;
+                conn.Open();
+
+                //select statement executed
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                //display it datagird view 
+                DataTable dt = new DataTable();
+                dt.Load(reader);
+
+                dataGridView1.DataSource = dt;
+
+                //configure the combox- name of combox is cmbSelect
+                cmbSelect.DisplayMember = "Airline";
+                cmbSelect.ValueMember = "ID";
+                cmbSelect.DataSource = dt;
+                reader.Close();
+            }
+
+            catch (Exception ex)
+            {
+                String msg = ex.Message.ToString();
+                String caption = "error";
+
+                MessageBox.Show(msg, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            finally
+            {
+                //cleans up all the memory in the ram
+                cmd.Dispose();
+                conn.Close();
+            }
+
+
+        }
+
+        private void btnAddNew_Click(object sender, EventArgs e)
+        {
+            string airline = tbAirline.Text;
+            string flight = tbFlightNum.Text;
+            string destination = tbDestination.Text;
+            string airplane = cmbAirlineType.SelectedItem.ToString();
+
+            if ((airline == " ") || (flight == " ") || (destination == " ") || (airplane == " "))
+            {
+                string msg = "No textbox can be empty ";
+                string caption = "Error !";
+                MessageBox.Show(caption, msg, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            else
+            {
+                conn.ConnectionString = conString;
+                cmd = conn.CreateCommand();
+            }
+
+            try
+            {
+                string query = "Insert into FlightInfo values ('"
+                    + airline + "' ,'"
+                    + flight + "','"
+                    + destination + "','"
+                    + airplane + "');";
+
+                cmd.CommandText = query;
+                conn.Open();
+                cmd.ExecuteScalar();
+                MessageBox.Show("New flight is added sucessfully", "Success!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+
+            catch (Exception ex)
+            {
+                String msg = ex.Message.ToString();
+                String caption = "Error!";
+                MessageBox.Show(caption, msg, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            finally
+            {
+                cmd.Dispose();
+                conn.Close();
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            refreshData();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int flID = Convert.ToInt32(cmbSelect.SelectedValue);
+
+            //retrieve flight name airline
+            String airline = tbAirline.Text;
+            String flight = tbFlightNum.Text;
+            String destination = tbDestination.Text;
+            String airplane = cmbAirlineType.SelectedItem.ToString();
+
+            if ((airline == " ") || (flight == " ") || (destination == " ") || (airplane == " "))
+            {
+                string msg = "No textbox can be empty ";
+                string caption = "Error !";
+                MessageBox.Show(caption, msg, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            else
+            {
+                //update flight info  set airline = '???', flightNum = '???', destination = '???', 
+                // airlineType = '???' where id = ???
+
+                String query = "Update  Flightinfo set Airline ='" + airline + "', FlightNum='" + flight + "', Destination='" + destination +
+                    "', AirplaneType='" + airplane + "'where ID=" + flID;
+
+                conn.ConnectionString = conString;
+                cmd = conn.CreateCommand();
+
+                try
+                {
+
+                    cmd.CommandText = query;
+                    conn.Open();
+                    cmd.ExecuteScalar();
+                }
+
+                catch (Exception ex)
+                {
+                    String msg = ex.Message.ToString();
+                    String caption = "Error!";
+                    MessageBox.Show(caption, msg, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                finally
+                {
+                    cmd.Dispose();
+                    conn.Close();
+                }
+            }
+
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            //delete from flightinfo where ID = ?? 
+            int flID = Convert.ToInt32(cmbSelect.SelectedValue);
+            conn.ConnectionString = conString;
+            cmd = conn.CreateCommand();
+
+            try
+            {
+
+                String query = "Delete from FlightInfo where ID=" + flID;
+                cmd.CommandText = query;
+                conn.Open();
+                cmd.ExecuteScalar();
+            }
+
+            catch (Exception ex)
+            {
+                String msg = ex.Message.ToString();
+                String caption = "Error!";
+                MessageBox.Show(caption, msg, MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+
+            finally
+            {
+                conn.Close();
+                cmd.Dispose();
+            }
+    }
+    }
+
+}
+
